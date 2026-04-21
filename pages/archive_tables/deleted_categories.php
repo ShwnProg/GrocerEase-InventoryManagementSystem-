960a055b8a2c7820b04a2476487bc4831614359e
Shawn Marlo Galdo <emailmo@gmail.com>
2026-04-21 11:32:09 +0800

    Add deleted categories and suppliers tables: implement recovery and delete actions for archived records

---
+++ pages/archive_tables/deleted_categories.php
@@ -0,0 +1,41 @@
+        <div class="menu-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Category Name</th>
+                            <th>Description</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1; ?>
+                        <?php foreach ($categories as $cat): ?>
+                            <?php if ($prod['is_deleted'] == 0)
+                                continue; ?>
+                            <tr>
+                                <td><?= $no++ ?></td>
+                                <td><?= htmlspecialchars($cat['category_name']) ?></td>
+                                <td><?= htmlspecialchars($prod['category_description']) ?></td>
+                                <td>
+                                    <div class="actions">
+                                        <form action="recover.php" method="POST">
+                                            <input type="hidden" name="product_id" value="<?= $prod['product_id_pk'] ?>">
+                                            <button type="submit" class="edit-btn">
+                                                <i class="fa-solid fa-recycle"></i>
+                                            </button>
+                                        </form>
+                                        <!-- DELETE ACTION -->
+                                        <form method="POST">
+                                            <input type="hidden" name="product_id" value="<?= $prod['product_id_pk'] ?>">
+                                            <button type="submit" name="delete_btn" class="edit-btn">
+                                                <i class="fa-solid fa-trash"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
