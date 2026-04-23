d043b2fba13ac9fe190ea06296db1dd5f5afb8f4
Shawn Marlo Galdo <emailmo@gmail.com>
2026-04-24 03:09:44 +0800

    Remove delete message handling: delete delete_message.php file

--- includes/delete_message.php
+++
@@ -1,6 +0,0 @@
-<?php if (!empty($delete_success)): ?>
-    <div class="success-message"><?= htmlspecialchars($delete_success) ?></div>
-<?php endif; ?>
-<?php if (!empty($delete_error)): ?>
-    <div class="error-message"><?= htmlspecialchars($delete_error) ?></div>
-<?php endif; ?>
